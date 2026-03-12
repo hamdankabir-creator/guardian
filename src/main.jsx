36b01577b8fb2426518789c60ad82f0d1c8adce1
hamdankabir-creator <hamdan.kabir@gmail.com>
2026-03-12 00:50:16 -0700

    add parent dashboard — ?view=parent route, broadcasts alerts from demo

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import GuardianDemo from "./GuardianDemo";
+import ParentDashboard from "./ParentDashboard";
+
+const view = new URLSearchParams(window.location.search).get("view");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GuardianDemo />
+    {view === "parent" ? <ParentDashboard /> : <GuardianDemo />}
   </React.StrictMode>
 );
